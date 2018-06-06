006e2e03ac36b63cb3eb6f159579c709dff033f2
Bryan Jones <thebeard@me.com>
2018-06-05 21:17:11 -0700

    Added backend validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Bryan:bryan1@ds247690.mlab.com:47690/devconnenctor"
+  mongoURI: "mongodb://Bryan:bryan1@ds247690.mlab.com:47690/devconnenctor",
+  secretOrKey: "secret"
 };
